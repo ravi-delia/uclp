5e9da94471244ec9b128cec39940832f6599c331
Ravi D'Elia <ravijdelia@gmail.com>
2024-03-25 20:54:24 -0400

    Added tests for the core functions

--- test/core-test.lisp
+++ test/core-test.lisp
@@ -18,3 +18,23 @@
   (signals uclp::undefined-rule
     (match :v "video"))
   (isnt-match :w " not w!"))
+
+(test compile-peg
+  (let ((comp-pat (compile-peg '(grammar
+				 :a (* "a" :b "a")
+				 :b (+ "bb" :a "|")
+				 :main :a))))
+    (is-match comp-pat "aaa|aaa") ; Can pass to match
+    (is-match `(split "," ,comp-pat) "a|a,abba,aa|aa"))) ; Can compose
+
+(test replace
+  (is (string= (replace-one '(* (set "Rr") "avi" (? "'s")) "John" "Ravi's name is ravi")
+	       "John name is ravi"))
+  (is (string= (replace-all '(* (set "Rr") "avi" (? "'s")) "John" "Ravi's name is ravi")
+	       "John name is John")))
+
+(test find
+  (let ((str "This is a secret waldo message with a man waldo hiding in waldo it"))
+    (is (match "waldo" str (find-one "waldo" str))) ; find-one should return int
+    (is (every (lambda (pos) (match "waldo" str pos)) ; find-all should return list
+	       (find-all "waldo" str)))))
